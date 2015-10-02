d55b4927e0377338616cd2dffad7777048626417
tfasz <todd@fuzzpi>
2015-10-02 15:21:18 +0000

    Light state

--- control.py
+++ control.py
@@ -13,11 +13,17 @@ from ouimeaux.environment import Environment  # install via: sudo pip install ou
 os.environ['TZ'] = 'US/Pacific'
 #print(sys.version)
 
+# Hard-code our location for the time being
+class Location:
+    tz = pytz.timezone('US/Pacific') 
+    obs = ephem.Observer()
+    obs.lat = '47.56'
+    obs.long = '-122.26'
+
+# Logic to normalize times in our rules to datetimes. This is both dealing
+# with calculating sunrise/sunsite (+/- offsets) and for parsing HH:MM times.
 class TimeCalc:	
-    pdt = pytz.timezone('US/Pacific') 
-    o = ephem.Observer()
-    o.lat = '47.561619'
-    o.long = '-122.269219'
+    loc = Location()
 
     def __init__(self, baseDate=None):
         if baseDate is None:
@@ -25,10 +31,10 @@ class TimeCalc:
         self.baseDate = self.floorMinute(baseDate)
         print("Base Date: " + str(self.baseDate))
         today = self.baseDate.replace(hour=0, minute=0)
-        todayOffset = today - self.pdt.utcoffset(self.baseDate)
-        self.o.date = ephem.Date(todayOffset)
-        self.sunrise = self.floorMinute(ephem.localtime(self.o.next_rising(ephem.Sun())))
-        self.sunset = self.floorMinute(ephem.localtime(self.o.next_setting(ephem.Sun())))
+        todayOffset = today - self.loc.tz.utcoffset(self.baseDate)
+        self.loc.obs.date = ephem.Date(todayOffset)
+        self.sunrise = self.floorMinute(ephem.localtime(self.loc.obs.next_rising(ephem.Sun())))
+        self.sunset = self.floorMinute(ephem.localtime(self.loc.obs.next_setting(ephem.Sun())))
         print "Sun up: " + str(self.sunrise) + " -> " + str(self.sunset)
    
     def floorMinute(self, date):
@@ -44,32 +50,64 @@ class TimeCalc:
     def getSunset(self, offset):
         return self.sunset + datetime.timedelta(minutes=offset) 
 
-class Light:
-    def __init__(self):
-        self.rules = []
-        self.expectedState
-        self.actualState
+    def active(self, timeOn, timeOff):
+        return (timeOn <= self.baseDate and timeOff > self.baseDate)
 
 class Rule:
-    calc = TimeCalc()
 
-    def __init__(self, ruleConfig):
+    def __init__(self, calc, ruleConfig):
         if 'on' in ruleConfig:
-            self.timeOn = self.calc.parseTime(ruleConfig['on']) 
+            self.timeOn = calc.parseTime(ruleConfig['on']) 
         elif 'onSunrise' in ruleConfig:
-            self.timeOn = self.calc.getSunrise(ruleConfig['onSunrise'])
+            self.timeOn = calc.getSunrise(ruleConfig['onSunrise'])
         elif 'onSunset' in ruleConfig:
-            self.timeOn = self.calc.getSunset(ruleConfig['onSunset'])
+            self.timeOn = calc.getSunset(ruleConfig['onSunset'])
 
         if 'off' in ruleConfig:
-            self.timeOff = self.calc.parseTime(ruleConfig['off']) 
+            self.timeOff = calc.parseTime(ruleConfig['off']) 
         elif 'offSunrise' in ruleConfig:
-            self.timeOff = self.calc.getSunrise(ruleConfig['offSunrise'])
+            self.timeOff = calc.getSunrise(ruleConfig['offSunrise'])
         elif 'offSunset' in ruleConfig:
-            self.timeOff = self.calc.getSunset(ruleConfig['offSunset'])
+            self.timeOff = calc.getSunset(ruleConfig['offSunset'])
+
+        # Decide if this rule is currently on/off
+        self.enabled = calc.active(self.timeOn, self.timeOff)
 
     def __str__(self):
-        return "Rule from " + str(self.timeOn) + " -> " + str(self.timeOff) 
+        return "Rule from " + str(self.timeOn) + " -> " + str(self.timeOff) + "  Enabled: " + str(self.enabled)
+
+class Light:
+    def __init__(self, name, calc, config):
+        self.name = name;
+        self.expectedOn = False
+
+        # For each rule we want to calc our on/off times
+        self.rules = []
+        for ruleConfig in config['rules']:
+            # Parse the ruleConfig for this rule
+            rule = Rule(calc, ruleConfig)
+            print rule
+            self.rules.append(rule)
+
+            # Light should be ON if any rule is enabled
+            if rule.enabled:
+                self.expectedOn = True
+
+    def __str__(self):
+        return "Light " + self.name + " - expectedOn: " + str(self.expectedOn) 
+
+class LightConfig:
+    calc = TimeCalc()
+
+    def __init__(self):
+        # Load our rule configuration
+        config = json.loads(open('rules.json').read())
+
+        # Loop through the config settings for all of our lights
+        for name, lightConfig in config.iteritems():
+            light = Light(name, self.calc, lightConfig)
+            print light
+
 
 def on_switch(switch):
     print 'on switch' 
@@ -91,18 +129,9 @@ def on_bridge(bridge):
 #env.start()
 
 def getRules():
-    # Load our rule configuration
-    lights = json.loads(open('rules.json').read())
+    # Parse rules into lights
+    lightConfig = LightConfig()
 
-    # Loop through the config settings for all of our lights
-    for name, lightConfig in lights.iteritems():
-        print name, ":", lightConfig
-
-        # For each rule we want to calc our on/off times
-        for ruleConfig in lightConfig['rules']:
-            # Parse the ruleConfig for this rule
-            rule = Rule(ruleConfig)
-            print rule
-
-
-getRules()
+# Run main if executed directly
+if __name__ == '__main__':
+    getRules()
